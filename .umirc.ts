722bff1b7da7694adb8fb4b8d5e0397a0a5511c5
Baic <303394539@qq.com>
2022-01-16 13:29:16 +0800

    Update .umirc.ts

--- .umirc.ts
+++ .umirc.ts
@@ -2,7 +2,7 @@ import defineConfig from '@baic/yolk-mobile-ui/lib/define-config';
 
 export default defineConfig({
   title: '凡人有喜',
-  publicPath: '/ordinary-miai/',
+  publicPath: '/fryx/',
   stylelint: true,
   strict: true,
   "h5+app": {},
